72d1a0dcebc7a3444c45599f48e8a3f0c94cc1e0
Sergey <sergeinickit@gmail.com>
2023-12-27 02:31:08 +0300

    добавил дз Sem4

--- Seminar4_Functions/obj/Debug/net8.0/Seminar4_Functions.AssemblyInfo.cs
+++ Seminar4_Functions/obj/Debug/net8.0/Seminar4_Functions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seminar4_Functions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a49cdd77411b5032a5206fbf4ee838d1a0e9c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seminar4_Functions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seminar4_Functions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
