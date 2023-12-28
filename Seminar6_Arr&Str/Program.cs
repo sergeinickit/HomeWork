0708ce2a76e41a1598b28df9b704647081775ba7
Sergey <sergeinickit@gmail.com>
2023-12-28 18:34:25 +0300

    добавил Program

--- Seminar6_Arr&Str/Program.cs
+++ Seminar6_Arr&Str/Program.cs
@@ -92,3 +92,51 @@
 // {
 //     return input.ToLower();
 // }
+
+
+
+// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+// Пример:   “aBcD1ef!-” => Нет
+//           “шалаш” => Да
+//           “55655” => Да
+
+Console.Write("Введите слово: ");      // Запрос у пользователя
+// #pragma warning disable CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
+string str = Console.ReadLine();       // Чтение введенной строки
+// #pragma warning restore CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
+if(IsPalindrome(str))
+{
+  Console.WriteLine("Да");
+}
+else
+{
+  Console.WriteLine("Нет");
+}
+
+// Console.WriteLine(str);                // Вывод строки
+char[] elemtnts = new char [str.Length];                                     // Создаем прерменную
+
+string GetStringFromArrayChar()     // Если нужен результат, который будем возвращать и передовать дальше заводим string
+{
+  string result="";                 // Создаем переменную типа string (пока пустая)
+  for (int i = 0; i < elemtnts.Length; i++)    // Заполняем переменную
+  {
+     result = result + elemtnts[i];   // В переменную str записываем каждый элеменет массива elemets[]  
+  }
+  return result;                   // Наша функция сработала и её результатом будет result
+}
+str = GetStringFromArrayChar();    // Заполняем тем, что получилось в нашей функции str = и дальше выводим
+System.Console.WriteLine();
+
+bool IsPalindrome(string str)       // Функция для прверки, является ли строка палиндромом
+{
+  str = str.ToLower();              // Приведение строки к нижнему регистру для учета регистра букв
+
+  str = str.Replace(" ","");        // Удаление пробелов из строки
+
+  string reversed = new string(str.ToCharArray().Reverse().ToArray());      // Создание обратной копии строки
+
+  return str == reversed;           // Сравнение исходной строки с ее обратной копией
+}
+
